2dce5eefc86c4f51358afda73a0a638442339c45
devopsdinosaur <devopsdinosaur@gmail.com>
2024-11-21 17:22:18 -0500

    ---

--- movement_speed/MovementSpeedPlugin.cs
+++ movement_speed/MovementSpeedPlugin.cs
@@ -3,6 +3,8 @@ using HarmonyLib;
 using System;
 using System.Reflection;
 using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
 
 public static class PluginInfo {
 
@@ -30,8 +32,17 @@ public static class PluginInfo {
 [BepInPlugin(PluginInfo.GUID, PluginInfo.TITLE, PluginInfo.VERSION)]
 public class MovementSpeedPlugin : DDPlugin {
     private Harmony m_harmony = new Harmony(PluginInfo.GUID);
-    private static float m_prev_multiplier = -1;
-
+    private static float m_original_default_speed = -1;
+    private static bool m_is_sprinting = false;
+    public static bool IsSprinting {
+        get {
+            return m_is_sprinting;
+        }
+        set {
+            m_is_sprinting = value;
+        }
+    }
+    
     private void Awake() {
         logger = this.Logger;
         try {
@@ -39,6 +50,7 @@ public class MovementSpeedPlugin : DDPlugin {
             Settings.Instance.load(this);
             DDPlugin.set_log_level(Settings.m_log_level.Value);
             this.create_nexus_page();
+            Hotkeys.load();
             this.m_harmony.PatchAll();
             logger.LogInfo($"{PluginInfo.GUID} v{PluginInfo.VERSION} loaded.");
         } catch (Exception e) {
@@ -46,12 +58,65 @@ public class MovementSpeedPlugin : DDPlugin {
         }
     }
 
+    [HarmonyPatch(typeof(LocalPlayerController), "Initialize")]
+    class HarmonyPatch_LocalPlayerController_Initialize {
+        private static void Postfix(LocalPlayerController __instance, PlayerInput ___m_input) {
+            try {
+                // https://docs.unity3d.com/Packages/com.unity.inputsystem@1.0/manual/Actions.html
+                ___m_input.actions.A
+            } catch (Exception e) {
+                logger.LogError("** HarmonyPatch_LocalPlayerController_Initialize.Prefix ERROR - " + e);
+            }
+        }
+    }
+
+    public class SprintChecker : MonoBehaviour {
+        private void OnGUI() {
+            try {
+                if (!Settings.m_enabled.Value || !Event.current.isKey) {
+                    return;
+                }
+                bool is_key_down = false;
+                foreach (KeyCode key in Hotkeys.HotKeys[Hotkeys.HOTKEY_SPRINT]) {
+                    if ((is_key_down = (Event.current.keyCode == key))) {
+                        break;
+                    }
+                }
+                if (Settings.m_hold_key_to_sprint.Value) {
+                    IsSprinting = is_key_down;
+                }
+            } catch (Exception e) {
+                logger.LogError("** SprintChecker.OnGUI ERROR - " + e);
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(PlayerMovementSpeed), "Awake")]
+    class HarmonyPatch_PlayerMovementSpeed_Awake {
+        private static void Postfix(PlayerMovementSpeed __instance) {
+            try {
+                __instance.gameObject.AddComponent<SprintChecker>();
+            } catch (Exception e) {
+                logger.LogError("** HarmonyPatch_PlayerMovementSpeed_Awake.Prefix ERROR - " + e);
+            }
+        }
+    }
+
     [HarmonyPatch(typeof(PlayerMovementSpeed), "Update")]
     class HarmonyPatch_PlayerMovementSpeed_Update {
+        private static bool m_last_sprint_value = true;
         private static bool Prefix(ref float ___m_defaultMovementSpeed) {
         	try {
-				if (Settings.m_enabled.Value && Settings.m_speed_multiplier.Value > 0 && Settings.m_speed_multiplier.Value != m_prev_multiplier) {
-                    ___m_defaultMovementSpeed *= (m_prev_multiplier = Settings.m_speed_multiplier.Value);
+                if (!Settings.m_enabled.Value || (Settings.m_speed_multiplier.Value <= 0 && Settings.m_sprint_speed_multiplier.Value <= 0)) {
+                    return true;
+                }
+                if (m_original_default_speed == -1) {
+                    m_original_default_speed = ___m_defaultMovementSpeed;
+                }
+                ___m_defaultMovementSpeed = m_original_default_speed * Settings.m_speed_multiplier.Value * (m_is_sprinting ? Settings.m_sprint_speed_multiplier.Value : 1f);
+                if (IsSprinting != m_last_sprint_value) {
+                    _debug_log($"[{Time.time}] movement_speed_multiplier: {Settings.m_speed_multiplier.Value}, is_sprinting: {IsSprinting}");
+                    m_last_sprint_value = IsSprinting;
                 }
                 return true;
 			} catch (Exception e) {
--- movement_speed/Settings.cs
+++ movement_speed/Settings.cs
@@ -16,6 +16,11 @@ public class Settings {
     public static ConfigEntry<bool> m_enabled;
     public static ConfigEntry<string> m_log_level;
     public static ConfigEntry<float> m_speed_multiplier;
+    public static ConfigEntry<float> m_sprint_speed_multiplier;
+    public static ConfigEntry<bool> m_hold_key_to_sprint;
+
+    // Hotkeys
+    public static ConfigEntry<string> m_hotkey_sprint;
 
     public void load(DDPlugin plugin) {
         this.m_plugin = plugin;
@@ -24,5 +29,10 @@ public class Settings {
         m_enabled = this.m_plugin.Config.Bind<bool>("General", "Enabled", true, "Set to false to disable this mod.");
         m_log_level = this.m_plugin.Config.Bind<string>("General", "Log Level", "info", "[Advanced] Logging level, one of: 'none' (no logging), 'error' (only errors), 'warn' (errors and warnings), 'info' (normal logging), 'debug' (extra log messages for debugging issues).  Not case sensitive [string, default info].  Debug level not recommended unless you're noticing issues with the mod.  Changes to this setting require an application restart.");
         m_speed_multiplier = this.m_plugin.Config.Bind<float>("General", "Movement Speed Multiplier", 1, "Multiplier applied to normal player movement (float, default 1 [no change]).");
+        m_sprint_speed_multiplier = this.m_plugin.Config.Bind<float>("General", "Sprint Speed Multiplier", 1.5f, "Multiplier applied (in addition to Movement Speed Multiplier) to movement speed (float, default 1.5).");
+        m_hold_key_to_sprint = this.m_plugin.Config.Bind<bool>("General", "Hold Sprint Key", true, "Set to false to cause sprint key to toggle sprint status.");
+
+        // Hotkeys
+        m_hotkey_sprint = this.m_plugin.Config.Bind<string>("Hotkeys", "Hotkey - Sprint", "LeftShift,RightShift", "Comma-separated list of Unity Keycodes, any of which will cause player to sprint (either toggle or held, depending on value of 'Hold Sprint Key' setting).");
     }
 }
